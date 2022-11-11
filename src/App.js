b983070de565b8639a554cea93f6065344d2c155
dembasow98 <dastech1998@gmail.com>
2022-11-11 15:18:10 +0300

    Tailwindcss framework configuration implemented successfully!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex w-full flex-col min-h-screen dark:bg-gray-900 place-items-center place-content-center">
+     <h1 className="font-bold text-3xl underline  text-blue-600"> Frontend initial setup and tailwind configuration done </h1>
+     <p className="text-slate-100">If really the Tailwind css framework is working, the above styles should be applied:</p>
+     <ol className="text-slate-100">
+        <li>The background color should be dark gray</li>
+        <li>Font should be bold</li>
+        <li>Text size should be 3xl</li>
+        <li>Text should be underlined</li>
+        <li>Text color should be blue</li>
+     </ol>
     </div>
   );
 }
