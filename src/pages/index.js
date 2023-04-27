5f5f832a06f979084b39d874edae3890b540f025
Zidan Omar Hamid <hzidanomar@gmail.com>
2023-04-27 15:33:46 +0300

    apply ts migration 🍛

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Dashboard } from './Dashboard/Dashboard';
-export { default as Profile } from './Profile/Profile';
-export { default as Bookshelf } from './Bookshelf/Bookshelf';
-export { default as Bookshelves } from './Bookshelves/Bookshelves';
-export { default as UserBookshelf } from './Profile/pages/UserBookshelf';
-export { default as UserForkshelf } from './Profile/pages/UserForkshelf';
-export { default as Error } from './Error/Error';
